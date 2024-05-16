28c8fa860e15a92be1a8e26d7454564d9ed382f2
Alan Rocha <141082245+alanrochagomes@users.noreply.github.com>
2024-05-16 16:30:26 +0000

    DB implementado no Update

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const { MongoClient, ObjectId } = require("mongodb");
 
-const dbUrl =
-  "mongodb+srv://admin:3au0j1F9GoaFWMMI@cluster0.hwppulk.mongodb.net";
+const dbUrl = "mongodb+srv://admin:3au0j1F9GoaFWMMI@cluster0.hwppulk.mongodb.net";
 const dbName = "ocean-jornada-backend-maio-2024";
 
 const client = new MongoClient(dbUrl);
@@ -70,7 +69,7 @@ async function main() {
   });
 
   // Endpoint de Update [PUT] /item/:id
-  app.put("/item/:id", function (req, res) {
+  app.put("/item/:id", async function (req, res) {
     // Acessar o ID do parâmetro de rota
     const id = req.params.id;
 
@@ -79,8 +78,11 @@ async function main() {
     const body = req.body;
     const atualizarItem = body.nome;
 
-    // Atualizar na lista o item recebido
-    itens[id - 1] = atualizarItem;
+    // Atualizar na collection o item recebido
+    await collection.updateOne(
+      { _id: new ObjectId(id) },
+      { $set: { nome: atualizarItem }}
+    )
 
     // Enviamos uma mensagem de sucesso
     res.send("Item atualizado com sucesso: " + id + ", " + atualizarItem);
